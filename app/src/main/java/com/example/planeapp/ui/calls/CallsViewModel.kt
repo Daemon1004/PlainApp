331f078a5c15a9ba64a8d4f2f716d9af92f94fce
Daemon <kirill.mihaylov.2002@list.ru>
2024-12-09 20:06:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/planeapp/ui/calls/CallsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.planeapp.ui.calls
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CallsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is calls Fragment"
+    }
+    val text: LiveData<String> = _text
+}
