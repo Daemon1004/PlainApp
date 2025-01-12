df447c8d6cdfc7f8c6b1a777271eb91895194bc0
Daemon <kirill.mihaylov.2002@list.ru>
2025-01-12 20:02:40 +0300

    test

--- app/src/main/java/com/example/plainapp/webrtc/peer/StreamPeerConnectionFactory.kt
+++ app/src/main/java/com/example/plainapp/webrtc/peer/StreamPeerConnectionFactory.kt
@@ -84,6 +84,7 @@ class StreamPeerConnectionFactory constructor(
   private val factory by lazy {
     PeerConnectionFactory.initialize(
       PeerConnectionFactory.InitializationOptions.builder(context)
+        .setFieldTrials("WebRTC-IgnoreLoopbackNetwork/Enabled/")
         .setInjectableLogger({ message, severity, label ->
           when (severity) {
             Logging.Severity.LS_VERBOSE -> {
